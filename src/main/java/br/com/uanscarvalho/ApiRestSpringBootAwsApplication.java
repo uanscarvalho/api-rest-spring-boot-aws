9f8b84c544af3dcaccf01d881b72c068445b0974
uanscarvalho <uanscarvalho@gmail.com>
2024-12-10 20:07:51 -0300

    Projeto Spring Boot com aws

---
+++ src/main/java/br/com/uanscarvalho/ApiRestSpringBootAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.uanscarvalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringBootAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestSpringBootAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/uanscarvalho/ApiRestSpringBootAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uanscarvalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBootAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
