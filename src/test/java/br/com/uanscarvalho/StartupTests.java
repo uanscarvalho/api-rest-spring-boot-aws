73af56be063fa03ccde207d514cf26ecf3f5c49c
uanscarvalho <uanscarvalho@gmail.com>
2024-12-12 11:46:08 -0300

    CRUD ainda sem conexão com banco

--- src/test/java/br/com/uanscarvalho/StartupTests.java
+++ src/test/java/br/com/uanscarvalho/StartupTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiRestSpringBootAwsApplicationTests {
+class StartupTests {
 
 	@Test
 	void contextLoads() {
